3c7228f66569daf554ab9495be38d450f0c4553a
Cole Moore <Cartisim@icloud.com>
2022-08-04 22:49:18 +0300

    Updated Packages

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["Dribble"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.41.1"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
